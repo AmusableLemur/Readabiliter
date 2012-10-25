832e02ac2640cf00eb9b995b1aecd0ada46f337d
Rasmus Larsson <meister@rasmuslarsson.se>
2012-10-25 20:46:21 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,58 @@
+<?php
+
+/**
+ *  Program to format and store articles and other documents
+ *  Copyright (C) 2012 Rasmus Larsson
+ *
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU General Public License
+ *  as published by the Free Software Foundation; either version 2
+ *  of the License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+require_once __DIR__.'/vendor/autoload.php';
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+$app = new Silex\Application();
+
+$app->register(new Silex\Provider\DoctrineServiceProvider(), array(
+    'db.options' => array(
+        'driver'   => 'pdo_sqlite',
+        'path'     => __DIR__.'/articles.db',
+    ),
+));
+
+$app->register(new Silex\Provider\TwigServiceProvider(), array(
+    'twig.path' => __DIR__.'/views',
+));
+
+$app->register(new Silex\Provider\UrlGeneratorServiceProvider());
+
+$app->error(function (\Exception $e, $code) {
+    return new Response('We are sorry, but something went terribly wrong.');
+});
+
+$app->get('/', function() {
+    return 'Hello!';
+})->bind('index');
+
+$app->post('/', function (Request $request) {
+    return new RedirectResponse('/');
+});
+
+$app->get('/article/{id}', function (Silex\Application $app, $id) {
+    return 'Hello!';
+})->assert('id', '\d+')->bind('article');
+
+$app->run();
